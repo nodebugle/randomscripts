55d59edf2f61ff1683867e903448531a44387b0a
nodebugle <steve@nodebug.co.uk>
2026-01-02 10:15:29 +0000

    Script to rename Dune Awakening startup movies and then start the game

---
+++ renameDuneStartupMovies.bat
@@ -0,0 +1,13 @@
+ren "C:\Games\Steam\steamapps\common\DuneAwakening\DuneSandbox\Content\Movies\StartupUE4.bk2" "C:\Games\Steam\steamapps\common\DuneAwakening\DuneSandbox\Content\Movies\StartupUE4.bak"
+
+ren "C:\Games\Steam\steamapps\common\DuneAwakening\DuneSandbox\Content\Movies\Logo_4K_Unreal_2s.bk2" "C:\Games\Steam\steamapps\common\DuneAwakening\DuneSandbox\Content\Movies\Logo_4K_Unreal_2s.bak"
+
+ren "C:\Games\Steam\steamapps\common\DuneAwakening\DuneSandbox\Content\Movies\Logo_4K_LevelInfinite_2s.bk2" "C:\Games\Steam\steamapps\common\DuneAwakening\DuneSandbox\Content\Movies\Logo_4K_LevelInfinite_2s.bak"
+
+ren "C:\Games\Steam\steamapps\common\DuneAwakening\DuneSandbox\Content\Movies\Logo_4K_Legendary_2s.bk2" "C:\Games\Steam\steamapps\common\DuneAwakening\DuneSandbox\Content\Movies\Logo_4K_Legendary_2s.bak"
+
+ren "C:\Games\Steam\steamapps\common\DuneAwakening\DuneSandbox\Content\Movies\Logo_4K_Funcom_2s.bk2" "C:\Games\Steam\steamapps\common\DuneAwakening\DuneSandbox\Content\Movies\Logo_4K_Funcom_2s.bak"
+
+ren "C:\Games\Steam\steamapps\common\DuneAwakening\DuneSandbox\Content\Movies\EpilepsyInfo.bk2" "C:\Games\Steam\steamapps\common\DuneAwakening\DuneSandbox\Content\Movies\EpilepsyInfo.bak"
+
+ren "C:\Games\Steam\steamapps\common\DuneAwakening\DuneSandbox\Content\Movies\IntroMovie\InitialIntro4k.bk2" "C:\Games\Steam\steamapps\common\DuneAwakening\DuneSandbox\Content\Movies\IntroMovie\InitialIntro4k.bak"
